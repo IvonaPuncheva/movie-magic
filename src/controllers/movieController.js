658ae6f48f4e9e6c6f33f0d45ad65b80d6fae9fe
IvonaPuncheva <ivpuncheva@gmail.com>
2024-06-14 13:11:41 +0300

    Add create movie page

---
+++ src/controllers/movieController.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+
+router.get('/create', (req, res) => {
+    res.render('create');
+});
+
+// router.get('/about', (req, res) =>{
+// res.render('about')
+// });
+
+module.exports = router;

de0c80d85d9e7093ecf9b20d5e693667a1db3b4e
IvonaPuncheva <ivpuncheva@gmail.com>
2024-06-14 14:41:14 +0300

    show dynamic movies on home page

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -7,7 +7,9 @@ router.get('/create', (req, res) => {
 
 router.post('/create', (req, res) => {
     const newMovie = req.body;
+
     movieService.create(newMovie);
+    
     res.send('Movie should be created');
 });
 
--- src/services/movieService.js
+++ src/services/movieService.js
@@ -1,4 +1,17 @@
-const movies = [];
+const movies = [{
+     title: 'fbfd',
+genre: 'dd',
+director: 'ee',
+date: '2019',
+imageUrl: '/img/jungle-cruise.jpeg',
+rating: '3',
+description: 'ef'
+}];
+
+exports.getAll =() =>{
+  return movies.slice();
+};
+
 
 exports.create = (movieData) =>{
   movies.push(movieData);
